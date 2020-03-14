afe31ce2efb25cfd70c4007a212960b0c2dda4c4
Jianu_Petrut <c.petrut97@gmail.com>
2020-03-14 13:40:33 +0200

    edit sleep time

--- main.py
+++ main.py
@@ -14,32 +14,32 @@ blue = 0x0000ff
 # Turn off heartbeat LED
 pycom.heartbeat(False)
 
-# # Initialize LoRaWAN radio
-# lora = LoRa(mode=LoRa.LORAWAN, region=LoRa.EU868)
+# Initialize LoRaWAN radio
+lora = LoRa(mode=LoRa.LORAWAN, region=LoRa.EU868)
 
-# # Set network keys
-# app_eui = binascii.unhexlify('70B3D57ED002B821')
-# app_key = binascii.unhexlify('BF2D68A0F3EB9A52633F93532D6C868E')
+# Set network keys
+app_eui = binascii.unhexlify('70B3D57ED002B821')
+app_key = binascii.unhexlify('BF2D68A0F3EB9A52633F93532D6C868E')
 
-# # # Join the network
-# lora.join(activation=LoRa.OTAA, auth=(app_eui, app_key), timeout=0)
-# pycom.rgbled(red)
+# # Join the network
+lora.join(activation=LoRa.OTAA, auth=(app_eui, app_key), timeout=0)
+pycom.rgbled(red)
 
-# # Loop until joined
-# while not lora.has_joined():
-#     print('Not joined yet...')
+# Loop until joined
+while not lora.has_joined():
+    print('Not joined yet...')
     
-#     pycom.rgbled(off)
-#     time.sleep(0.1)
-#     pycom.rgbled(red)
-#     time.sleep(2)
+    pycom.rgbled(off)
+    time.sleep(0.1)
+    pycom.rgbled(red)
+    time.sleep(2)
 
-# print('Joined')
-# pycom.rgbled(blue)
+print('Joined')
+pycom.rgbled(blue)
 
-# s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
-# s.setsockopt(socket.SOL_LORA, socket.SO_DR, 5)
-# s.setblocking(True)
+s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
+s.setsockopt(socket.SOL_LORA, socket.SO_DR, 5)
+s.setblocking(True)
 
 
 adc = machine.ADC()             # create an ADC object
@@ -50,7 +50,7 @@ gate_pin = adc.channel(pin='P17', attn=adc.ATTN_0DB)
 
 
 while True:
-    #value = '{ \"audio\":\'' + str(audio_pin.value()) + '\'}'
+    
     #get sound detector values
     audio =audio_pin.value() 
     envelope = envelope_pin.value()
@@ -64,8 +64,8 @@ while True:
     #send them
     json_payload=json.dumps(payload)
     print(json_payload)
-    # s.send(json_payload)
-    time.sleep(0.5)     
+    s.send(json_payload)
+    time.sleep(2.5)     
 
 
 
